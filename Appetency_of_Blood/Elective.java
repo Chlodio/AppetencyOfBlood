7c8d592466cd7ebf955d66aca11d0282faf0c2de
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 12:51:08 +0200

    Delete Elective.java

--- Appetency_of_Blood/Elective.java
+++
@@ -1,9 +0,0 @@
-class Elective extends Succession {
-	public static boolean callHeir(){
-		Succession.doMaintenance();
-		Succession.heir = 			House.getMagnates().get(0);
-		Succession.special =		1;
-		Succession.inform();
-		return true;
-	}
-}
