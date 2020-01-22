fd452ba30d52c7608a52c04f29d050d2d696110e
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 12:49:57 +0200

    Delete Appointment.java

--- Appetency_of_Blood/Appointment.java
+++
@@ -1,9 +0,0 @@
-class Appointment extends Succession {
-	public static boolean callHeir(Human appointer, Human appointed){
-		Succession.doMaintenance();
-		Succession.heir = 			appointed;
-		Succession.special =		1;
-		Succession.inform();
-		return true;
-	}
-}
