aab25f4cccd8ec768745985ceef2249c179dcc08
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 14:35:37 +0200

    Delete Elective.java

--- Common/Elective.java
+++
@@ -1,9 +0,0 @@
-class Elective {
-	public static boolean callHeir(){
-		Succession.doMaintenance();
-		Succession.heir = 			Basic.choice(House.getMagnates());
-		Succession.special =		1;
-		Succession.inform();
-		return true;
-	}
-}
