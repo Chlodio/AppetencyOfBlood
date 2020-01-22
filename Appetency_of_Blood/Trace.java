09f9bc658db6e25858965d7ef6ea5d0ae8edbdc0
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 12:58:54 +0200

    Delete Trace.java

--- Appetency_of_Blood/Trace.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Trace {
-	public static int collateral(Human human){
-		List<List<Human>> list = new ArrayList<List<Human>>();
-
-	}
-}
