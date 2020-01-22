f2af006329d17e1fc044ecaf2071d489611c7894
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 12:58:10 +0200

    Delete SuccessionLaw.java

--- Appetency_of_Blood/SuccessionLaw.java
+++
@@ -1,9 +0,0 @@
-public class SuccessionLaw{
-	private Succession succession;
-	private int coverture;
-
-	public SuccessionLaw(){
-		this.succession = new AgnaticCognaticPrimogeniture();
-		this.coverture = 0;
-	}
-}
