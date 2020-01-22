05b33c7f9034e76f223df4c152be62795db07f83
Chlodio <21118398+Chlodio@users.noreply.github.com>
2020-01-22 12:51:23 +0200

    Delete Fief.java

--- Appetency_of_Blood/Fief.java
+++
@@ -1,8 +0,0 @@
-//Non-sovereign territory
-public class Fief extends Territory{
-	private Realm realm; 			//part of a realm
-	public Fief(Office office, Realm realm){
-		super(office);
-		this.realm = realm;
-	}
-}
